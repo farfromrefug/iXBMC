1a8528dd70d679f46b3ba556ef669cf41026b1d1
Martin Guillon <mguillon@mmovea-mac.movea.local>
2011-04-02 15:54:35 +0200

    Initial Commit

---
+++ iXBMC/SecondViewController.h
@@ -0,0 +1,16 @@
+//
+//  SecondViewController.h
+//  iXBMC
+//
+//  Created by Martin Guillon on 4/2/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface SecondViewController : UIViewController {
+    
+}
+
+@end
